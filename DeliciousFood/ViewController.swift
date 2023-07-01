104b588cf861fd8e523bbf9a3ed5128010c1ba44
Ruslan <hellperock@gmail.com>
2023-07-01 11:44:34 +0500

    Initial Commit

---
+++ DeliciousFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeliciousFood
+//
+//  Created by Ruslan Ismailov on 01/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
